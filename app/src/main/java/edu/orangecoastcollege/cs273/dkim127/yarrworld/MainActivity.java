b41a6c79397f4c5f2206f11439f68f9a3152856a
DirgeForge <dhaustinkim@gmail.com>
2016-09-02 21:40:53 -0700

    code from the final minute of the lecture

--- app/src/main/java/edu/orangecoastcollege/cs273/dkim127/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dkim127/yarrworld/MainActivity.java
@@ -2,8 +2,10 @@ package edu.orangecoastcollege.cs273.dkim127.yarrworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button speakButton = (Button) findViewById(R.id.speakButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // inflating (!) opening the layout called activity_main (activity_main.xml)
         setContentView(R.layout.activity_main);
+
     }
+
 }
